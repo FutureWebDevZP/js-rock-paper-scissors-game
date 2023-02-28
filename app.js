98bb8288f89c8f111662c1d8552c3646bcfb9d8e
Zuri P <108591374+FutureWebDevZP@users.noreply.github.com>
2023-02-27 23:18:20 -0500

    updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // pick out all the elements with ID's
 
-const computerChoice = document.getElementById('computer-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
@@ -12,13 +12,47 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 // by now here the user choice buttons are working.
 
 function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3) // or you can use possibleChoices.length
-    console.log(randomNumber)
+    const randomNumber = Math.floor(Math.random() * 3) + 1 // or you can use possibleChoices.length
 
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
 }
 
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'its a draw!'
+    }
+    if (computerChoice === 'rock' && userChoice === "paper") {
+        result = 'you win!'
+    }
+    if (computerChoice === 'rock' && userChoice === "scissors") {
+        result = 'you lose!'
+    }
+    if (computerChoice === 'paper' && userChoice === "rock") {
+        result = 'you lose!'
+    }
+    if (computerChoice === 'paper' && userChoice == 'scissors') {
+        result = 'you win!'
+    }
+    if (computerChoice === 'scissors' && userChoice == 'paper') {
+        result = 'you lose!'
+    }
+    if (computerChoice === 'scissors' && userChoice == 'rock') {
+        result = 'you win!'
+    }
+    resultDisplay.innerHTML = result
+}
