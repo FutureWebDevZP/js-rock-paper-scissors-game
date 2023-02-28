d0611a7fba3f79403ed571b5b7ab8213616b80a3
Zuri P <108591374+FutureWebDevZP@users.noreply.github.com>
2023-02-27 22:55:07 -0500

    updated

--- app.js
+++ app.js
@@ -7,9 +7,18 @@ const possibleChoices = document.querySelectorAll('button')
 let userChoice
 
 // get all the possible choices and have JS listen for a click
+// these are funtions, this is what makes the JS code work
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
 }))
 
 // by now here the user choice buttons are working.
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) // or you can use possibleChoices.length
+    console.log(randomNumber)
+
+}
+
